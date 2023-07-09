60a3b0d90474d3c45ce0b1c12f522a34e12cdd18
Franck P <franckpoingt@gmail.com>
2023-07-09 17:38:43 +1200

    make vercel happy

--- src/lib/contentful-client.ts
+++ src/lib/contentful-client.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'contentful';
+import * as contentful from 'contentful';
 
-export const client = createClient({
+export const client = contentful.createClient({
 	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
 	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
 });
