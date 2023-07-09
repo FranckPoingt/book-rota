8b98ceb93bbc01abb2699cf336c37550b1a04281
Franck P <franckpoingt@gmail.com>
2023-07-09 17:44:45 +1200

    hacky fix

--- src/lib/contentful-client.ts
+++ src/lib/contentful-client.ts
@@ -1,6 +1,12 @@
-import * as contentful from 'contentful';
+import { createClient } from 'contentful';
+import contentful from 'contentful';
 
-export const client = contentful.createClient({
+const config = {
 	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
 	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
-});
+};
+
+const createClientFunc =
+	process.env.NODE_ENV === 'development' ? createClient : contentful.createClient;
+
+export const client = createClientFunc(config);
